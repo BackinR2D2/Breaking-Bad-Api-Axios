6a2af985d846dd24aa7057b876a8adf2ee4dae8e
BackinR2D2 <cristimlg334@gmail.com>
2020-08-11 19:22:10 +0300

    back to top scroll

---
+++ public/hp.js
@@ -0,0 +1,13 @@
+const btn = $('#button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
